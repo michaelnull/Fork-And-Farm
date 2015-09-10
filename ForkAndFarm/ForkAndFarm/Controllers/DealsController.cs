a770b1852bb752ea35a8eca2070c7cb56d5ef361
Michael Null <michael.null@bpmw.net>
2015-09-09 19:43:02 -0500

    fixed a lot of links and logic, added a couple of links to all ads

--- ForkAndFarm/ForkAndFarm/Controllers/DealsController.cs
+++ ForkAndFarm/ForkAndFarm/Controllers/DealsController.cs
@@ -16,29 +16,30 @@ namespace ForkAndFarm.Controllers
 
         // GET: Deals/Create
         [Authorize]
-        public ActionResult ProposePurchase(int? id)
+        public ActionResult ProposeDeal(int? id)
         {
             if (id == null)
             {
-                return RedirectToAction("Index","SupplyOffers");
+                return RedirectToAction("Index","Advertisements");
             }
-            Advertisement supplyoffer = db.Advertisements.FirstOrDefault(x => x.Id == id);
+            Advertisement offer = db.Advertisements.FirstOrDefault(x => x.Id == id);
             Deal deal = new Deal();
             ForkAndFarmUser currentuser = db.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
-            if (currentuser.UserRole == ForkAndFarmUser.Portal.Purchaser)
+            if ((currentuser.UserRole == ForkAndFarmUser.Portal.Purchaser && offer.AdType == AdType.SupplyOffer) || 
+                (currentuser.UserRole == ForkAndFarmUser.Portal.Supplier && offer.AdType == AdType.PurchaseOffer))
             {
                 deal.ProposedBy = currentuser.UserName;
                 deal.ProposedByOrganization = currentuser.Organization;
                 deal.ProposedByPhone = currentuser.Phone;
-                deal.Delivery = supplyoffer.Delivery;
-                deal.PaymentTerms = supplyoffer.PaymentTerms;
-                deal.Product = supplyoffer.Product;
-                deal.Quantity = supplyoffer.Quantity;
-                deal.Unit = supplyoffer.Unit;
-                deal.UnitPrice = supplyoffer.UnitPrice;
-                deal.OfferedTo = supplyoffer.ProposedBy;
-                deal.OfferId = supplyoffer.Id;
-                deal.ExtPrice = supplyoffer.UnitPrice * supplyoffer.Quantity;
+                deal.Delivery = offer.Delivery;
+                deal.PaymentTerms = offer.PaymentTerms;
+                deal.Product = offer.Product;
+                deal.Quantity = offer.Quantity;
+                deal.Unit = offer.Unit;
+                deal.UnitPrice = offer.UnitPrice;
+                deal.OfferedTo = offer.ProposedBy;
+                deal.OfferId = offer.Id;
+                deal.ExtPrice = offer.UnitPrice * offer.Quantity;
                 return View(deal);
             }
             else
@@ -54,15 +55,11 @@ namespace ForkAndFarm.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult ProposePurchase(Deal deal)
+        public ActionResult ProposeDeal(Deal deal)
         {
-            var supplyoffer = db.Advertisements.FirstOrDefault(x => x.Id == deal.OfferId);
-            if (supplyoffer == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var offeree = db.Users.FirstOrDefault(x=>x.UserName == supplyoffer.ProposedBy);
 
+            var offeree = db.Users.FirstOrDefault(x => x.UserName == deal.OfferedTo);
+            var offer = db.Advertisements.FirstOrDefault(x => x.Id == deal.OfferId);
             var currentuser = db.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
             
             deal.CreatedOn = DateTime.Now;
@@ -74,7 +71,7 @@ namespace ForkAndFarm.Controllers
 
                 currentuser.DealsFromMe.Add(deal);
                 offeree.DealsToMe.Add(deal);
-                supplyoffer.ResponseToAdvertisement.Add(deal);
+                offer.ResponseToAdvertisement.Add(deal);
                 db.SaveChanges();
                 
                 return RedirectToAction("ShowProposedDeals", new { id = deal.OfferId });
@@ -94,95 +91,16 @@ namespace ForkAndFarm.Controllers
             ViewBag.User = User.Identity.Name;
            
             var offer = db.Advertisements.FirstOrDefault(x => x.Id == id);
-            return View(offer.ResponseToAdvertisement.OrderByDescending(x => x.CreatedOn));
-
-
-        }
-        // GET: Deals/Create
-        [Authorize]
-        public ActionResult ProposeSale(int? id)
-        {
-            if (id == null)
+            var list = offer.ResponseToAdvertisement.OrderByDescending(x => x.CreatedOn);
+            if (list.Count() > 0)
             {
-                return RedirectToAction("Index", "PurchaseOffers");
-            }
-            Advertisement purchaseoffer = db.Advertisements.FirstOrDefault(x => x.Id == id);
-            Deal deal = new Deal();
-            ForkAndFarmUser currentuser = db.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
-            if(currentuser.UserRole == ForkAndFarmUser.Portal.Supplier)
-            {
-                deal.ProposedBy = currentuser.UserName;
-                deal.ProposedByOrganization = currentuser.Organization;
-                deal.ProposedByPhone = currentuser.Phone;
-                deal.Delivery = purchaseoffer.Delivery;
-                deal.PaymentTerms = purchaseoffer.PaymentTerms;
-                deal.Product = purchaseoffer.Product;
-                deal.Quantity = purchaseoffer.Quantity;
-                deal.Unit = purchaseoffer.Unit;
-                deal.UnitPrice = purchaseoffer.UnitPrice;
-                deal.OfferedTo = purchaseoffer.ProposedBy;
-                deal.OfferId = purchaseoffer.Id;
-                deal.ExtPrice = purchaseoffer.Quantity * purchaseoffer.UnitPrice;
-                return View(deal);
+                return View(list);
             }
             else
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("Details", "Advertisements", new { id = offer.Id });
             }
-        }
-
-        // POST: Deals/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ProposeSale(Deal deal)
-        {
-            var purchaseoffer = db.Advertisements.FirstOrDefault(x => x.Id == deal.OfferId);
-            if (purchaseoffer == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var offeree = db.Users.FirstOrDefault(x => x.UserName == purchaseoffer.ProposedBy);
-
-            var currentuser = db.Users.FirstOrDefault(x => x.UserName == User.Identity.Name);
-            
-            deal.CreatedOn = DateTime.Now;
-            deal.ExtPrice = deal.Quantity * deal.UnitPrice;
-            if (ModelState.IsValid)
-            {
-                db.Deals.Add(deal);
-
-                currentuser.DealsFromMe.Add(deal);
-                offeree.DealsToMe.Add(deal);
-                purchaseoffer.ResponseToAdvertisement.Add(deal);
-                db.SaveChanges();
-
-                return RedirectToAction("ShowProposedSaleDeals", new { id = deal.OfferId });
-            }
-
-
-            return View(deal);
-        }
-
-        // GET: Deals
-        public ActionResult ShowProposedSaleDeals(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var offer = db.Advertisements.FirstOrDefault(x => x.Id == id);
-
-            ViewBag.User = User.Identity.Name;
-            return View(offer.ResponseToAdvertisement.OrderByDescending(x => x.CreatedOn));
-        }
-
-        // GET: Deals
-        public ActionResult Index()
-        {
-            return View(db.Deals.ToList());
+           
         }
 
         // GET: Deals/Details/5
@@ -199,22 +117,6 @@ namespace ForkAndFarm.Controllers
             }
             return View(deal);
         }
-        // GET: Deals/Details/5
-        public ActionResult PurchaseDetails(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Deal deal = db.Deals.Find(id);
-            if (deal == null)
-            {
-                return HttpNotFound();
-            }
-            return View(deal);
-        }
-
-     
 
         // GET: Deals/Edit/5
         [Authorize]
@@ -291,10 +193,12 @@ namespace ForkAndFarm.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+
             Deal deal = db.Deals.Find(id);
+            int offerid = deal.OfferId;
             db.Deals.Remove(deal);
             db.SaveChanges();
-            return RedirectToAction("ShowProposedDeals", new { id = deal.OfferId });
+            return RedirectToAction("ShowProposedDeals", new { id = offerid });
         }
 
         protected override void Dispose(bool disposing)
@@ -306,77 +210,77 @@ namespace ForkAndFarm.Controllers
             base.Dispose(disposing);
         }
 
-        // GET: Deals/Edit/5
-        [Authorize]
 
-        public ActionResult PurchaseEdit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
 
-            Deal deal = db.Deals.Find(id);
-            if (deal == null)
-            {
-                return HttpNotFound();
-            }
-            if (deal.ProposedBy == User.Identity.Name)
-            {
-                return View(deal);
-            }
-            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        }
 
-        // POST: Deals/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult PurchaseEdit(Deal deal)
-        {
-            deal.ExtPrice = deal.UnitPrice * deal.Quantity;
-            deal.Memo = "edited on " + DateTime.Now + " " + deal.Memo;
-            var offerId = deal.OfferId;
-            if (ModelState.IsValid)
-            {
-                db.Entry(deal).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("ShowProposedSaleDeals", new { id = offerId });
-            }
-            return View(deal);
-        }
 
-        // GET: Deals/Delete/5
-        [Authorize]
-        public ActionResult PurchaseDelete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Deal deal = db.Deals.Find(id);
-            if (deal == null)
-            {
-                return HttpNotFound();
-            }
-            if (deal.ProposedBy == User.Identity.Name)
-            {
-                return View(deal);
-            }
-            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        }
 
-        // POST: Deals/Delete/5
-        [HttpPost, ActionName("PurchaseDelete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult PurchaseDeleteConfirmed(int id)
-        {
-            Deal deal = db.Deals.Find(id);
-            db.Deals.Remove(deal);
-            db.SaveChanges();
-            return RedirectToAction("ShowProposedSaleDeals", new { id = deal.OfferId });
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                      
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+                                                                                      
 
        
     }
